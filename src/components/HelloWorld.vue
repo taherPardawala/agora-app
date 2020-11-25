681af243317d583ae8516c5b4378c80fc228eb1a
taherPardawala <skypunch2@gmail.com>
2020-11-25 17:06:51 +0530

    integrate changes and make the video play

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,138 +1,69 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
-          target="_blank"
-          rel="noopener"
-          >pwa</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+	<div class="videoWrapper">
+		<div v-if="primaryStreamId == domId" class="videoButtonWrapper">
+			<button @click="muteAudio">{{ audio ? "Mute" : "UnMute" }}</button>
+			<button @click="muteVideo">{{ video ? "Mute Video" : "UnMute Video" }}</button>
+			<button @click="exitMeeting">Exit Meeting</button>
+		</div>
+		<div class="videoItemWrapper" ref="player" :id="domId"></div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
-  }
-};
+	export default {
+		name: "HelloWorld",
+		data() {
+			return {
+				audio: true,
+				video: true
+			};
+		},
+		mounted() {
+			this.$nextTick(function() {
+				console.log(this.stream);
+				if (this.stream && !this.stream.isPlaying()) {
+					this.stream.play(`${this.domId}`, { fit: "cover" }, err => {
+						if (err && err.status !== "aborted") {
+							console.warn("trigger autoplay policy");
+						}
+						console.log(err);
+					});
+				}
+			});
+		},
+		methods: {
+			muteAudio() {
+				this.audio = !this.audio;
+				this.$emit("setAudioStatus", this.audio);
+			},
+			muteVideo() {
+				this.video = !this.video;
+				this.$emit("setVideoStatus", this.video);
+			},
+			exitMeeting() {
+				this.$emit("jaraHuMai");
+			}
+		},
+		beforeDestroy() {
+			if (this.stream) {
+				if (this.stream.isPlaying()) {
+					this.stream.stop();
+				}
+				this.stream.close();
+			}
+		},
+		props: ["domId", "stream", "primaryStreamId"]
+	};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss">
+	.videoItemWrapper {
+		height: 100vh;
+		width: auto;
+
+		video {
+			left: 0;
+		}
+	}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,178 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div class="meetingSectionWrapper">
+		<div v-if="!jointMeeting" class="meetingDetails">
+			<input v-model="appid" type="text" placeholder="enter appid" />
+			<input v-model="channel" type="text" placeholder="enter channel name" />
+			<button @click="startMeeting">Join Meeting</button>
+		</div>
+		<div v-if="jointMeeting" class="primaryStreamWrapper">
+			<HelloWorld
+				@setVideoStatus="updateVideoStatus"
+				@setAudioStatus="updateAudioStatus"
+				@jaraHuMai="leaveMeeting"
+				:primaryStreamId="primaryStream.id"
+				:stream="primaryStream.stream"
+				:domId="primaryStream.id"
+			/>
+		</div>
+		<div class="remoteStreamWrapper">
+			<HelloWorld
+				v-for="stream of secondaryStream"
+				:primaryStreamId="primaryStream.id"
+				:stream="stream.stream"
+				:domId="stream.id"
+				:key="stream.id"
+			/>
+		</div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+	// @ is an alias to /src
+	import HelloWorld from "@/components/HelloWorld.vue";
+	import RTCClient from "../helpers/AgoraClientWrapper";
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+	export default {
+		name: "Home",
+		components: {
+			HelloWorld
+		},
+		async created() {
+			this.rtc = new RTCClient();
+			let rtc = this.rtc;
+
+			if (rtc.testSystemRequirements()) {
+				let devices = await rtc.testDevices();
+
+				if (devices.audioDevices && !devices.audioDevices.length) {
+					this.rtc.videoStreamConfig.audio = false;
+				}
+				if (devices.videoDevices && !devices.videoDevices.length) {
+					this.rtc.videoStreamConfig.video = false;
+				}
+
+				// write additional code here
+				rtc.on("stream-added", evt => {
+					let { stream } = evt;
+					console.log("[agora] [stream-added] stream-added", stream.getId());
+					rtc.client.subscribe(stream);
+				});
+
+				rtc.on("stream-subscribed", evt => {
+					let { stream } = evt;
+					console.log("[agora] [stream-subscribed] stream-added", stream.getId());
+					if (!this.secondaryStream.find(it => it.id === stream.getId())) {
+						this.secondaryStream.push({ stream, id: stream.getId() });
+					}
+				});
+
+				rtc.on("stream-removed", evt => {
+					let { stream } = evt;
+					console.log("[agora] [stream-removed] stream-removed", stream.getId());
+					this.secondaryStream = this.secondaryStream.filter(it => it.id !== stream.getId());
+				});
+
+				rtc.on("peer-online", evt => {
+					console.log(`PEER ${evt.uid} IS ONLINE`);
+				});
+
+				rtc.on("peer-leave", evt => {
+					console.log(`PEER ${evt.uid} ALREADY LEFT`);
+					this.secondaryStream = this.secondaryStream.filter(it => it.id !== evt.uid);
+				});
+
+				rtc.on("mute-audio", function(evt) {
+					console.log("Remote stream: " + evt.uid + "has muted audio");
+				});
+
+				rtc.on("unmute-audio", function(evt) {
+					console.log("Remote stream: " + evt.uid + "has muted audio");
+				});
+
+				// show user icon whenever a remote has disabled their video
+				rtc.on("mute-video", function(evt) {
+					console.log("Remote stream: " + evt.uid + "has muted video");
+				});
+
+				rtc.on("unmute-video", function(evt) {
+					console.log("Remote stream: " + evt.uid + "has un-muted video");
+				});
+			} else {
+				console.log("Browser badal Garib.");
+			}
+		},
+		data() {
+			return {
+				jointMeeting: false,
+				primaryStream: {
+					id: "",
+					stream: ""
+				},
+				localStream: {
+					id: "",
+					stream: ""
+				},
+				secondaryStream: [],
+				rtc: false,
+				appid: "824340cbb84f429283930d38448d5d08",
+				channel: "meraphelebarhai1"
+			};
+		},
+		methods: {
+			startMeeting() {
+				this.rtc
+					.joinChannel({
+						appid: this.appid,
+						channel: this.channel
+					})
+					.then(() => {
+						console.log("JOIN HO GAYA SUCCESS");
+						this.rtc
+							.publishStream()
+							.then(stream => {
+								console.log("PUBLISH STREAM HO GAYA SUCCESS", stream);
+								this.primaryStream.stream = this.localStream.stream = stream;
+								this.primaryStream.id = this.localStream.id = stream.getId();
+								this.jointMeeting = true;
+							})
+							.catch(err => {
+								console.log("publish local error", err);
+							});
+					})
+					.catch(err => {
+						console.log("join channel error", err);
+					});
+			},
+			updateVideoStatus(status) {
+				if (!status) {
+					this.rtc.localStream.camera.stream.muteVideo();
+				} else {
+					this.rtc.localStream.camera.stream.unmuteVideo();
+				}
+			},
+			updateAudioStatus(status) {
+				if (!status) {
+					this.rtc.localStream.camera.stream.muteAudio();
+				} else {
+					this.rtc.localStream.camera.stream.unmuteAudio();
+				}
+			},
+			leaveMeeting() {
+				this.jointMeeting = false;
+				this.rtc
+					.leaveChannel()
+					.then(() => {
+						console.log("Leave Success");
+					})
+					.catch(err => {
+						console.log("Leave Failure");
+					});
+				this.primaryStream = this.localStream = {
+					id: "",
+					stream: ""
+				};
+				this.secondaryStream = [];
+			}
+		}
+	};
 </script>
